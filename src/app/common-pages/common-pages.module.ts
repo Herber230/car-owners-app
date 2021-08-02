5d5b7feebd545ab21f25c0bd46e68cdc5e31df9e
Sergio Gomez <ingguillermo@hotmail.es>
2021-08-01 23:18:30 -0600

    feat: Integracion de tabla hecha por SERGIO GUILLERMO GOMEZ MENDOZA

--- src/app/common-pages/common-pages.module.ts
+++ src/app/common-pages/common-pages.module.ts
@@ -3,26 +3,12 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { CommonLayoutComponent } from './common-layout/common-layout.component';
 import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
 
 
 import { MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatListModule} from '@angular/material/list';
 import {MatIconModule} from '@angular/material/icon';
-import {MatCardModule} from '@angular/material/card'; 
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-
-const MaterialModules = [
-  MatSidenavModule,
-  MatToolbarModule,
-  MatListModule,
-  MatIconModule,
-  MatCardModule,
-  MatInputModule,
-  MatButtonModule
-]
 
 @NgModule({
   declarations: [
@@ -32,8 +18,10 @@ const MaterialModules = [
   imports: [
     CommonModule,
     RouterModule,
-    ReactiveFormsModule,
-    ...MaterialModules
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatIconModule
   ]
 })
 export class CommonPagesModule { }
--- src/app/common-pages/login/login.component.ts
+++ src/app/common-pages/login/login.component.ts
@@ -1,92 +1,15 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Subscription } from 'rxjs';
-
-import { LoginService } from '../services/login.service';
-import { AuthService } from '../services/auth.service';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
+  styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit, OnDestroy {
-  loginForm: FormGroup;
-  private _loginError?: string;
+export class LoginComponent implements OnInit {
 
-  private _userChangeSubscription?: Subscription;
-  private _passwordChangeSubscription?: Subscription;
+  constructor() { }
 
-  constructor(
-    private loginService: LoginService,
-    private authService: AuthService,
-    private router: Router
-  ) {
-    this.loginForm = new FormGroup({
-      user: new FormControl('', [Validators.required]),
-      password: new FormControl('', [Validators.required]),
-    });
-
-    const cleanSubscriber = {
-      next: () => this.cleanError(),
-    };
-    this._userChangeSubscription = this.loginForm
-      .get('user')
-      ?.valueChanges.subscribe(cleanSubscriber);
-    this._passwordChangeSubscription = this.loginForm
-      .get('password')
-      ?.valueChanges.subscribe(cleanSubscriber);
+  ngOnInit(): void {
   }
 
-  ngOnInit(): void {}
-
-  ngOnDestroy(): void {
-    this._userChangeSubscription?.unsubscribe();
-    this._passwordChangeSubscription?.unsubscribe();
-  }
-
-  onSubmit() {
-    this.loginService
-      .authenticate(
-        this.loginForm.get('user')?.value,
-        this.loginForm.get('password')?.value
-      )
-      .subscribe({
-        next: (result) => {
-          if (result.success) {
-            this.authService.accessToken = result.token;
-            this.authService.refreshToken = result.refreshToken;
-            this.router.navigate(['owners']);
-          } else {
-            this.setError('Usuario o contraseña invalidos');
-          }
-        },
-        error: () => {
-          this.setError('Error de la aplicacion');
-        },
-      });
-  }
-
-  setError(message: string) {
-    this.loginForm.get('user')?.setValue('');
-    this.loginForm.get('password')?.setValue('');
-    this._loginError = message;
-  }
-
-  cleanError() {
-    this._loginError = '';
-  }
-
-  get loginError() {
-    return this._loginError;
-  }
-
-  get user() {
-    return this.loginForm.get('user')?.value;
-  }
-
-  get password() {
-    return this.loginForm.get('password')?.value;
-  }
 }
